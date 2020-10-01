e62e3495e84600af3c27f3925f44e0ec7353fa67
Soumyadip Daripa <soumyadipdaripa100@gmail.com>
2020-10-01 20:07:20 +0530

    Create promise.js

---
+++ promise.js
@@ -0,0 +1,17 @@
+var promise = new Promise(function(resolve, reject) { 
+  const x = "geeksforgeeks"; 
+  const y = "geeksforgeeks"
+  if(x === y) { 
+    resolve(); 
+  } else { 
+    reject(); 
+  } 
+}); 
+  
+promise. 
+    then(function () { 
+        console.log('Success, You are a GEEK'); 
+    }). 
+    catch(function () { 
+        console.log('Some error has occured'); 
+    }); 
